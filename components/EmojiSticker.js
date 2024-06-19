cd9b418880086cbed0e4e08e87f714aa793d7017
erick-sudo <erickochieng766@gmail.com>
2024-06-19 23:59:01 +0300

    Modal, and FlatList

---
+++ components/EmojiSticker.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Image, View } from "react-native";
+
+export default function EmojiSticker({ stickerSource, imageSize }) {
+  return (
+    <View style={{ top: -350 }}>
+      <Image
+        source={stickerSource}
+        resizeMode="contain"
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  );
+}
